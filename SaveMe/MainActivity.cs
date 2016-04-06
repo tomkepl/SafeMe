8128f6233fb83b636e2e6fd514a68ca1041b3cc7
Tomek <tomkepl@gmail.com>
2016-04-06 18:38:51 +0200

    test

--- SaveMe/MainActivity.cs
+++ SaveMe/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Android.App;
 using Android.Content;
+using Android.Hardware;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
@@ -9,9 +10,13 @@ using Android.OS;
 namespace SaveMe
 {
     [Activity(Label = "SaveMe", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, ISensorEventListener
     {
-        int count = 1;
+        static readonly object _syncLock = new object();
+        SensorManager _sensorManagerAcc;
+        SensorManager _sensorManagerGir;
+        TextView _sensorTextViewAcc;
+        TextView _sensorTextViewGir;        
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -23,8 +28,50 @@ namespace SaveMe
             // Get our button from the layout resource,
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += delegate { StartSensors(); };
+            _sensorTextViewAcc = FindViewById<TextView>(Resource.Id.textView2);
+            _sensorTextViewGir = FindViewById<TextView>(Resource.Id.textView4);
+        }
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        private void StartSensors()
+        {
+            _sensorManagerAcc = (SensorManager)GetSystemService(SensorService);
+            _sensorManagerGir = (SensorManager)GetSystemService(SensorService);
+        }
+
+        public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
+        {
+            // We don't want to do anything here.
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            lock (_syncLock)
+            {
+                if (e.Sensor.Type == SensorType.Accelerometer)
+                {
+                    _sensorTextViewAcc.Text = $"x={e.Values[0]:f}, y={e.Values[1]:f}, y={e.Values[2]:f}";
+                }
+                if (e.Sensor.Type == SensorType.Gyroscope)
+                {
+                    _sensorTextViewGir.Text = $"x={e.Values[0]:f}, y={e.Values[1]:f}, y={e.Values[2]:f}";
+                }
+
+            }
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _sensorManagerAcc.RegisterListener(this, _sensorManagerAcc.GetDefaultSensor(SensorType.Accelerometer), SensorDelay.Fastest);
+            _sensorManagerGir.RegisterListener(this, _sensorManagerGir.GetDefaultSensor(SensorType.Gyroscope), SensorDelay.Fastest);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _sensorManagerAcc.UnregisterListener(this);
+            _sensorManagerGir.UnregisterListener(this);
         }
     }
 }
--- SaveMe/Resources/Resource.Designer.cs
+++ SaveMe/Resources/Resource.Designer.cs
@@ -63,6 +63,18 @@ namespace SaveMe
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
 			
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView3 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int textView4 = 2131034116;
+			
 			static Id()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
